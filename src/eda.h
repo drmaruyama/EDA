029f352d99f30cf1508f7a0b3f87f38ea1e4b8ed
Yutaka MARUYAMA <drmaruyama@gmail.com>
2017-01-12 11:49:40 +0900

    initial commit

---
+++ src/eda.h
@@ -0,0 +1,7 @@
+#ifndef EDA_H
+#define EDA_H
+
+class EDA {
+};
+
+#endif
---
+++ src/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include "eda.h"
+
+int main (int argc, char * argv[]) {
+  EDA * system;
+
+  system = new EDA;
+
+  if (argc == 1) {
+    std::cout << "No parameter file!" << std::endl;
+    return (1);
+  } 
+
+  if (argc > 2) {
+    std::cout << "Too much arguments!" << std::endl;
+    return(1);
+  }
+
+//  system -> initialize(argv[1]);
+//  system -> iterate();
+//  system -> output();    
+
+  return(0);
+}
